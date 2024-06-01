3a5b5e510a496f8011d935b63cd201c404c3dbed
roshan <roshan.dev@al-ghurair.com>
2024-06-01 15:55:21 +0400

    fixed build issue

--- build.js
+++ build.js
@@ -1,9 +1,3 @@
 import fs from 'fs/promises';
 
-let paths = ['dist'];
-const cwd = process.cwd().split('/').at(-1);
-if(cwd != 'motu'){
-    paths = paths.concat(['src', 'tsconfig.json'])
-}
-
-paths.forEach(e => fs.rm(e, {force: true, recursive: true}).catch(console.error));
+fs.rm('dist', {force: true, recursive: true}).catch(console.error)
